1876769efa203865f44ef553b0f695666e767962
Rene Lopez Cano <rene.lopez.cano@gmail.com>
2014-11-29 00:47:37 -0600

    Added Dish List

--- Catering.Data/Repositories/Contracts/IDishRepository.cs
+++ Catering.Data/Repositories/Contracts/IDishRepository.cs
@@ -1,8 +1,13 @@
-﻿using Catering.Data.Models;
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Catering.Data.Models;
 
 namespace Catering.Data.Repositories.Contracts
 {
     public interface IDishRepository:IGenericRepository<Dish>
     {
+        Task<int> GenerateIdAsync();
+        Task<bool> ExistsAsync(Expression<Func<Dish, bool>> predicate);
     }
 }
--- Catering.Data/Repositories/Implementation/DishRepository.cs
+++ Catering.Data/Repositories/Implementation/DishRepository.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 using Catering.Data.DataLayer;
 using Catering.Data.Models;
 using Catering.Data.Repositories.Contracts;
@@ -9,6 +12,17 @@ namespace Catering.Data.Repositories.Implementation
     {
         public DishRepository(DbContext context) : base(context)
         {
+        }
+
+        public async Task<int> GenerateIdAsync()
+        {
+            var max=await _dbSet.MaxAsync(el => (int?) el.Id) ?? 0;
+            return 1 + max;
+        }
+
+        public async Task<bool> ExistsAsync(Expression<Func<Dish, bool>> predicate)
+        {
+            return await _dbSet.AnyAsync(predicate);
         }
     }
 }
